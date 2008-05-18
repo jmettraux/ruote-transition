61a10b0d987577a91c59b0ea1fe07977b07c6082
John Mettraux <jmettraux@gmail.com>
2008-05-18 18:29:12 +0900

    initial YAWL parser implementation

--- README.txt
+++ README.txt
@@ -1,18 +1,39 @@
 
 = ruote-transition
 
+
 == overview
 
 a small project about turning place-transition based process definitions to OpenWFEru process definitions.
+
+For now, is able to parse XPDL (http://www.wfmc.org/standards/xpdl.htm) and YAWL (http://www.yawl-system.com/)
+
+== get it
+
+    git clone git://github.com/jmettraux/ruote-transition.git
+
+(depends on OpenWFEru - http://github.com/jmettraux/ruote)
+
+
+== run it
+
+    ./bin/to_owfe -h
+
+    ./bin/to_owfe -o ruby -i xpdl test/xpdl/troubleticket.xpdl
+    ./bin/to_owfe -o xml -i yawl test/yawl/sample.yawl
+    ./bin/to_owfe -o dot -i xpdl test/xpdl/troubleticket.xpdl
+
 
 == Links
 
 http://openwferu.rubyforge.org
 http://rubyforge.org/projects/openwferu
 
+
 == license
 
 BSD
+
 
 == feedback
 
--- lib/openwfe/trans/yawl.rb
+++ lib/openwfe/trans/yawl.rb
@@ -46,7 +46,7 @@ require 'openwfe/trans/graph'
 
 module OpenWFE::Trans
 
-  module XPDL
+  module YAWL
 
     def self.parse (filename)
 
@@ -60,67 +60,45 @@ module OpenWFE::Trans
 
         graph = nil
         place = nil
-        transition = nil
-        t_restriction = nil
-        performer = nil
+
+        transition_id = 0
 
         # registering the listeners
 
         # u, l, q, a <=> url, local, qname, attributes
 
-        parser.listen(:start_element, ['WorkflowProcess']) do |u, l, q, a|
+        parser.listen(:start_element, ['processControlElements']) do |u, l, q, a|
           graph = Graph.new
           graphs << graph
         end
 
-        #parser.listen(:end_element, ['WorkflowProcess']) do |u, l, q, a|
-        #end
+        parser.listen(:start_element, ['inputCondition', 'outputCondition']) do |u, l, q, a|
 
-        parser.listen(:start_element, ['Activity']) do |u, l, q, a|
-
-          place = Place.new a['Id'], a['Name'], l, a
+          place = Place.new a['id'], a['id'], l, a
           graph << place
         end
 
-        parser.listen(:start_element, ['Split', 'Join']) do |u, l, q, a|
+        parser.listen(:start_element, ['task']) do |u, l, q, a|
 
-          t_restriction = [ l.downcase.to_sym, a['Type'].downcase.to_sym ]
+          place = Place.new a['id'], a['id'], l, a
+          graph << place
         end
 
-        parser.listen(:start_element, ['TransitionRef']) do |u, l, q, a|
+        parser.listen(:start_element, ['nextElementRef']) do |u, l, q, a|
 
-          place.transition_details[a['Id']] = t_restriction #.dup() ...
-        end
+          tid = "t_#{transition_id}"
 
-        parser.listen(:start_element, ['Transition']) do |u, l, q, a|
+          transition = Transition.new tid, tid, l, a
 
-          transition = Transition.new a['Id'], a['Name'], l, a
-          transition.from = a['From']
-          transition.to = a['To']
+          transition_id += 1
+
+          transition.from = place.eid
+          transition.to = a['id']
+
           graph << transition
         end
 
-        parser.listen(:start_element, ['Condition']) do |u, l, q, a|
-
-          transition.condition = { :type => a['Type'] }
-        end
-
-        parser.listen(:start_element, ['Performer']) do |u, l, q, a|
-
-          performer = true
-        end
-
         parser.listen(:characters) do |text| # very minimalistic...
-
-          if performer
-
-            place.attributes['participant'] = text
-            performer = nil
-
-          elsif transition and transition.condition and text.strip != ""
-
-            transition.condition[:expression] = text \
-          end
         end
 
         # parse now
